49bb13542192f951c3a959cf0b1b87eb5035fa56
cienfleur <redvelvet0214@protonmail.ch>
2025-11-05 10:45:27 +0000

    Implemented basic REST API functionality

--- lib/assignment-stack.ts
+++ lib/assignment-stack.ts
@@ -1,16 +1,79 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
+import { generateBatch } from "../shared/util";
+import { movies } from "../seed/movies";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AssignmentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Tables 
+    const moviesTable = new dynamodb.Table(this, "MoviesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "Movies",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AssignmentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    
+    // Functions 
+    const getMovieByIdFn = new lambdanode.NodejsFunction(
+      this,
+      "GetMovieByIdFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getMovieById.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: cdk.Aws.REGION,
+        },
+      }
+      );
+      
+      const getAllMoviesFn = new lambdanode.NodejsFunction(
+        this,
+        "GetAllMoviesFn",
+        {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_18_X,
+          entry: `${__dirname}/../lambdas/getAllMovies.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: cdk.Aws.REGION,
+          },
+        }
+        );
+        
+        new custom.AwsCustomResource(this, "moviesddbInitData", {
+          onCreate: {
+            service: "DynamoDB",
+            action: "batchWriteItem",
+            parameters: {
+              RequestItems: {
+                [moviesTable.tableName]: generateBatch(movies),
+              },
+            },
+            physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"), //.of(Date.now().toString()),
+          },
+          policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+            resources: [moviesTable.tableArn],
+          }),
+        });
+        
+        // Permissions 
+        moviesTable.grantReadData(getMovieByIdFn)
+        moviesTable.grantReadData(getAllMoviesFn)
+}
+
 }
