07f13c23fbcd0f37f0285a2ec4b362aab1219e1b
cienfleur <redvelvet0214@protonmail.ch>
2025-11-12 16:18:38 +0000

    feature - signin and signout resources

--- lib/cognito-stack.ts
+++ lib/cognito-stack.ts
@@ -46,6 +46,16 @@ export class CognitoStack extends cdk.Stack {
     "SignupFn",
     'signup.ts'
     );
+
+    this.addAuthRoute(
+      "confirm_signup",
+      "POST",
+      "ConfirmFn",
+      'confirm-signup.ts'
+    );
+
+    this.addAuthRoute('signout', 'GET', 'SignoutFn', 'signout.ts');
+    this.addAuthRoute('signin', 'POST', 'SigninFn', 'signin.ts');
   }
 
   private addAuthRoute(
