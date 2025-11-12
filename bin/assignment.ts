6343663de86704365fce1e6f6286088a5657467a
cienfleur <redvelvet0214@protonmail.ch>
2025-11-12 15:36:56 +0000

    feature - implement endpoint for signing up

--- bin/assignment.ts
+++ bin/assignment.ts
@@ -2,8 +2,12 @@
 import "source-map-support/register";
 import * as cdk from 'aws-cdk-lib';
 import { AssignmentStack } from '../lib/assignment-stack';
+import { CognitoStack } from "../lib/cognito-stack";
 
 const app = new cdk.App();
 new AssignmentStack(app, 'AssignmentStack', {
+  env: { region: 'eu-west-1' },
+});
+new CognitoStack(app, 'CognitoStack', {
   env: { region: 'eu-west-1' },
 });
--- lib/cognito-stack.ts
+++ lib/cognito-stack.ts
@@ -39,6 +39,13 @@ export class CognitoStack extends cdk.Stack {
     });
 
     this.auth = authApi.root.addResource("auth");
+
+    this.addAuthRoute(
+    "signup",
+    "POST",
+    "SignupFn",
+    'signup.ts'
+    );
   }
 
   private addAuthRoute(
@@ -49,25 +56,25 @@ export class CognitoStack extends cdk.Stack {
     allowCognitoAccess?: boolean
   ): void {
     const commonFnProps = {
-        architecture: lambda.Architecture.ARM_64,
-        timeout: cdk.Duration.seconds(10),
-        memorySize: 128,
-        runtime: lambda.Runtime.NODEJS_18_X,
-        handler: "handler",
-        environment: {
-          USER_POOL_ID: this.userPoolId,
-          USER_POOL_CLIENT_ID: this.userPoolClientId,
-          REGION: cdk.Aws.REGION,
-        },
-      };
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "handler",
+      environment: {
+        USER_POOL_ID: this.userPoolId,
+        CLIENT_ID: this.userPoolClientId,
+        REGION: cdk.Aws.REGION
+      },
+    };
+    
+    const resource = this.auth.addResource(resourceName);
+    
+    const fn = new node.NodejsFunction(this, fnName, {
+      ...commonFnProps,
+      entry: `${__dirname}/../lambdas/auth/${fnEntry}`,
+    });
 
-      const resource = this.auth.addResource(resourceName);
-
-      const fn = new node.NodejsFunction(this, fnName, {
-        ...commonFnProps,
-        entry: fnEntry,
-      });
-
-      resource.addMethod(method, new apig.LambdaIntegration(fn));
-    }
-  }
+    resource.addMethod(method, new apig.LambdaIntegration(fn));
+  }  // end private method
+}
